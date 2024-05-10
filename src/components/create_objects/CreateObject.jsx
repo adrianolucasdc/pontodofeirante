27d91a665c32074916272bd824f8043239e677f9
adrianolucasdc <adrianocaixeta5@gmail.com>
2024-05-10 01:08:47 -0300

    feat: add CreateObject.jsx for DB

---
+++ src/components/create_objects/CreateObject.jsx
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client"
+
+
+const prisma = new PrismaClient()
+
+async function criarUsuario(nome, email, senha, telefone, cpf, cep, dataNasc, sexo, 
+    estado, cidade, rua, numero, bairro){
+        const create_user = await prisma.usuario.create({
+            data:{
+                nome : nome,
+                email : email,
+                senha : senha,
+                telefone : telefone,
+                cpf : cpf,
+                dataNasc : dataNasc+"T12:00:00Z",
+                sexo : sexo,
+                endereco : {
+                    create : {
+                        cep : cep,
+                        uf : estado,
+                        cidade : cidade, 
+                        rua : rua,
+                        numero : numero,
+                        bairro : bairro,
+                    }
+                } 
+
+            }
+        })
+    }
+
