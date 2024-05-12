e63b6619ee4271fb8ed9f1dd069c106fd5ec9e90
adrianolucasdc <adrianocaixeta5@gmail.com>
2024-05-11 21:05:13 -0300

    feat: novo cadastro implementado com formik

--- src/pages/cadastro_user/cadastro_user.jsx
+++ src/pages/cadastro_user/cadastro_user.jsx
@@ -1,43 +1,46 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
-
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage, validateYupSchema} from 'formik';
+import * as Yup from "yup";
 import { IMaskInput } from "react-imask";
-import Dropdown_cidades from "../../components/forms/dropdown_cidades"
-import Dropdown_estados from "../../components/forms/dropdown_estados"
-import Menu_principal from "../../components/menu_principal/menu_principal"
 
+
+
+
+import Menu_principal from "../../components/menu_principal/menu_principal";
+import campoPreencher from "../../components/formularios/input";
+import criarUsuario from '../../components/create_objects/CreateObject';
+import { PrismaClient } from '@prisma/client';
+
+
+
+
+
+const validationSchema = Yup.object().shape({
+    nome: Yup.string().required("* Campo obrigatório!"),
+    email: Yup.string().email("* Insira um email válido!").required("* Campo obrigatório!"),
+    termos: Yup.boolean().isTrue("* É necessário aceitar os termos de condições!"),
+    senha: Yup.string().required("* Campo obrigatório!").min(8, "* A senha deve conter no mínimo 8 caracteres"),
+    telefone: Yup.string().required("* Campo obrigatório!").min(15, "* Insira um telefone válido!"),
+    cpf: Yup.string().required("* Campo obrigatório!"),
+    dataNasc: Yup.date()
+    .min(new Date(2000,0,1), "* Para fazer o cadastro é necessário ter mais de 10 anos!")
+    .required("* Campo obrigatório!"),
+    sexo: Yup.string().required("* Campo obrigatório!"),
+    uf: Yup.string().required("* Campo obrigatório!"),
+    cidade: Yup.string().required("* Campo obrigatório!"),
+    bairro: Yup.string().required("* Campo obrigatório!"),
+    rua:Yup.string().required("* Campo obrigatório!"),
+    cep: Yup.string().required("* Campo obrigatório!"),
+    numero: Yup.string().required("* Campo obrigatório!"), 
+    confirmSenha: Yup.string().oneOf([Yup.ref("senha"), null], "* As senhas devem corresponder!")
+    .required("* Confirmação de senha é obrigatório!"), 
+})
 
 
 export default function Cadastro_user() {
 
-    const [formValues, setFormValues] = useState({});
-
-    const handleInputChange = (e) => {
-        e.preventDefault();
-        const { value, name } = e.target;
-        setFormValues({ ...formValues, [name]: value })
-    }
-
-    const [nomeUser, setNomeUser] = useState('');
-    const [emailUser, setEmailUser] = useState('');
-    const [passwordUser, setPasswordUser] = useState('');
-    const [confirmPasswordUser, setConfirmPasswordUser] = useState('');
-    const [phoneUser, setPhoneUser] = useState('');
-    const [cpfUser, setCpfUser] = useState('');
-    const [cepUser, setCepUser] = useState('');
-    const [dtNascimentoUser, setDtNascimentoUser] = useState('');
-    const [sexoUser, setSexoUser] = useState('');
-    const [estadoUser, setEstadoUser] = useState('');
-    const [cidadeUser, setCidadeUser] = useState('');
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        if (nomeUser !== '' && emailUser !== '' && passwordUser !== '' && confirmPasswordUser !== '' && phoneUser !== '' && cpfUser !== '' && cepUser !== '' && dtNascimentoUser !== '' && sexoUser !== '') {
-            alert("Enviou os dados")
-        }
-    }
-
-
+    const [submitted, setSubmitted] = useState(false);
 
     return (
         <div className="">
@@ -46,135 +49,139 @@ export default function Cadastro_user() {
                     <svg className=" fill-primaryColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z" /></svg>
                 </Link>
             </div>
-
             <div className="  flex justify-center items-center mt-5">
+                
                 <div className=" flex flex-col items-center justify-center w-80 mb-32 px-5 rounded-xl border-2 border-primaryColor shadow-2xl">
                     <h1 className=" font-extrabold text-4xl text-primaryColor mt-6">Cadastre seu usuário</h1>
+                    <div className="flex flex-col  mt-6 h-full w-full mx-3">
+                        <Formik 
+                        initialValues={{nome : "", email : "", senha: "", confirmSenha: "", telefone: "", cpf : "", dataNasc : "", sexo: "", 
+                        cep: "", uf: "", cidade : "", rua : "", numero: "", bairro: "", termos: false }}
+                        onSubmit={values => {
+                            // criarUsuario(values.nome, values.email, values.senha, values.telefone, values.cpf,
+                            // values.cep, values.dataNasc, values.sexo,values.uf, values.cidade, values.rua,
+                            // values.numero, values.bairro)
+                        console.log(values.email)
+                        setSubmitted(true)
+                        setTimeout(()=>{
+                            setSubmitted(false)
+                        }, 3000)}}
+                        // validationSchema={validationSchema}
+                        >
+                        
+                            {({handleSubmit}) => (
+                                <Form onSubmit={handleSubmit}>
+                                    {campoPreencher("Nome Completo:","Insira seu nome...","nome","text")}
+                                    {campoPreencher("E-mail: ","Insira seu e-mail...","email","email")}
+                                    {campoPreencher("Senha: ", "Insira sua senha...", "senha", "password")}
+                                    {campoPreencher("Confirme sua senha: ", "Repita sua senha...", "confirmSenha", "password")}
+                                    <div className=" mb-3">
+                                        <h1 className=" text-primaryColor">Número de celular: </h1>
+                                        <Field name="telefone" type="text">
+                                        {
+                                            ({field}) => (
+                                            <IMaskInput {...field}
+                                            name="telefone"
+                                            mask="(00) 00000-0000"
+                                            placeholder='Insira seu celular...'
+                                            className=" border-primaryColor border-2 rounded-md w-full h-9 pl-2"
+                                            />
+                                        )}
+                                        </Field>
 
-                    <div className=" flex flex-col  mt-6 h-full w-full mx-3">
-                        <form onSubmit={handleSubmit}>
-                            <div className=" mb-3">
-                                <h1 className=" text-primaryColor">Nome completo</h1>
-                                <input
-                                    id="nomeUser"
-                                    value={nomeUser}
-                                    onChange={(e) => setNomeUser(e.target.value)}
-                                    type="text"
-                                    className=" border-primaryColor border-2 rounded-md w-full h-9 pl-2" />
-                            </div>
+                                        <ErrorMessage name='telefone' component="div" className='error text-red-600 text-sm ml-3'/>
+                                    </div>
+                                    <div className=" mb-3">
+                                        <h1 className=" text-primaryColor">Insira seu CPF:</h1>
+                                        <Field name="cpf">{({field}) => (
+                                            <IMaskInput {...field}
+                                            name="cpf"
+                                            mask="000.000.000-00"
+                                            placeholder='Insira seu CPF...'
+                                            className=" border-primaryColor border-2 rounded-md w-full h-9 pl-2"
+                                            />
+                                        )}</Field>
+                                        <ErrorMessage name='cpf' component="div" className='error text-red-600 text-sm ml-3'/>
+                                    </div>
+                                    
+                                    <div className='flex justify-between mb-3'>
+                                        <div className='w-[80%]'>
+                                            <span>Data de Nascimento: </span>
+                                            <Field type="date" name="dataNasc" className="pl-2 w-11/12 h-9 border-primaryColor border-2 rounded-md"/>
+                                            <ErrorMessage name='dataNasc' component="div" className='error text-red-600 text-sm ml-3'/>
+                                        </div>
+                                        <div className='overflow-hidden w-[47%]'>
+                                            <span className='block'>Sexo: </span>
+                                            <Field as="select" name="sexo" className=" h-9 border-primaryColor border-2 rounded-md">
+                                                <option value="">Selecione</option>
+                                                <option value="Masculino">Masculino</option>
+                                                <option value="Feminino">Feminino</option>
+                                            </Field>
+                                            
+                                            <ErrorMessage name='sexo' component="div" className='error text-red-600 text-sm ml-3'/>
+                                            
+                                            
+                                        </div>
+                                    </div>
 
-                            <div className=" mb-3">
-                                <h1 className=" text-primaryColor" >E-mail</h1>
-                                <input
-                                    id="emailUser"
-                                    value={emailUser}
-                                    onChange={(e) => setEmailUser(e.target.value)}
-                                    type="email"
-                                    className=" border-primaryColor border-2 rounded-md w-full h-9 pl-2" />
-                            </div>
+                                    <div className=' mb-3 mt-7 py-1 text-center bg-gradient-to-r from-lineColor from-10% via-zinc-300 via-50% to-lineColor to-90% rounded-md'>
+                                        <span className='font-extrabold text-lg'>Endereço</span>
+                                    </div>
 
-                            <div className=" mb-3">
-                                <h1 className=" text-primaryColor">Senha</h1>
-                                <input
-                                    id="passwordUser"
-                                    value={passwordUser}
-                                    onChange={(e) => setPasswordUser(e.target.value)}
-                                    type="password"
-                                    className=" border-primaryColor border-2 rounded-md w-full h-9 pl-2" />
-                            </div>
+                                    <div className="flex justify-between">
+                                        <div>
+                                            <h1 className=" text-primaryColor">Insira seu CEP:</h1>
+                                            <Field name="cep">{({field}) => (
+                                                <IMaskInput {...field}
+                                                name="cep"
+                                                mask="00000-000"
+                                                placeholder='Insira seu CEP...'
+                                                className=" border-primaryColor border-2 rounded-md w-full h-9 pl-2"
+                                                />
+                                            )}</Field>
+                                            <ErrorMessage name='cep' component="div" className='error text-red-600 text-sm ml-3'/>
+                                        </div>
+                                        <div className='w-36 overflow-hidden ml-4'>
+                                            {campoPreencher("Número: ", "Insira o número...","numero")}
+                                        </div>
+                                    </div>
 
-                            <div className=" mb-3">
-                                <h1 className=" text-primaryColor">Confirme sua senha</h1>
-                                <input
-                                    id="confirmPasswordUser"
-                                    value={confirmPasswordUser}
-                                    onChange={(e) => setConfirmPasswordUser(e.target.value)}
-                                    type="password"
-                                    className=" border-primaryColor border-2 rounded-md w-full h-9 pl-2" />
-                            </div>
+                                    {campoPreencher("Estado: ","Insira seu estado...", "uf", "text")}
+                                    {campoPreencher("Cidade: ", "Insira sua cidade...","cidade","text")}
+                                    {campoPreencher("Bairro: ","Insira seu bairro...","bairro","text")}
+                                    {campoPreencher("Rua: ", "Insira sua rua...","rua")}
 
-                            <div className=" mb-3">
-                                <h1 className=" text-primaryColor">Número de telefone</h1>
-                                <IMaskInput
-                                    id="phoneUser"
-                                    value={phoneUser}
-                                    onChange={(e) => setPhoneUser(e.target.value)}
-                                    mask="(00) 00000-0000"
-                                    className=" border-primaryColor border-2 rounded-md w-full h-9 pl-2"
-                                />
-                            </div>
+                                    <div className="h-3 "></div>
 
-                            <div className=" mb-3">
-                                <h1 className=" text-primaryColor">CPF</h1>
-                                <IMaskInput
-                                    id="cpfUser"
-                                    value={cpfUser}
-                                    onChange={(e) => setCpfUser(e.target.value)}
-                                    mask="000.000.000-00"
-                                    className=" border-primaryColor border-2 rounded-md w-full h-9 pl-2"
-                                />
-                            </div>
+                                    <div className='mb-2'>
+                                        <label>
+                                            <Field type='checkbox' name="termos" className="mx-2"/>
+                                            Aceito os
+                                            <Link to="/termos_user">
+                                                <span className='ml-1 text-linkColor underline visited:text-blue-950'>Termos de Condições</span>
+                                            </Link>
+                                            <ErrorMessage name='termos' component="div" className='error text-red-600 text-sm ml-3'/>
+                                        </label>
+                                    </div>
+                                    <div className='flex justify-center mb-4'>
+                                        <button type='submit' className=" h-9 w-36 bg-secundaryColor rounded-xl active:bg-primaryColor active:text-secundaryColor">Enviar</button>
+                                    </div>
+                                    {submitted && 
+                                    <div>
+                                        <div className="fixed  px-5 pt-1  w-[75%] h-[40px] bg-lime-200 rounded-md border-green-800 border-2 opacity-[0.9] left-1/2 top-[8%] translate-x-[-50%] translate-y-[-50%]">
+                                            <span className='text-lime-600 text-center'>Usuário criado com sucesso!</span>
+                                        </div>
+                                    </div>}
+                                </Form>
 
-                            <div className=" mb-3">
-                                <h1 className=" text-primaryColor">CEP</h1>
-                                <IMaskInput
-                                    id="cepUser"
-                                    value={cepUser}
-                                    onChange={(e) => setCepUser(e.target.value)}
-                                    mask="00000-000"
-                                    className=" border-primaryColor border-2 rounded-md w-full h-9 pl-2"
-                                />
-                            </div>
-
-                            <div className=" mb-3 flex flex-row justify-between">
-                                <div>
-                                    <h1 className=" text-primaryColor">Data de nascimento</h1>
-                                    <input
-                                        id="dtNascimentoUser"
-                                        value={dtNascimentoUser}
-                                        onChange={(e) => setDtNascimentoUser(e.target.value)}
-                                        type="date"
-                                        className=" border-primaryColor border-2 rounded-md w-32 h-9 pl-2">
-                                    </input>
-                                </div>
-
-                                <div>
-                                    <h1 className=" text-primaryColor">Sexo</h1>
-                                    <select
-                                        className=" h-9 border-primaryColor border-2 rounded-md"
-                                        name="sexo"
-                                        id="sexo"
-                                        value={sexoUser}
-                                        onChange={(e) => setSexoUser(e.target.value)}>
-                                        
-                                        <option value="masculino">Masculino</option>
-                                        <option value="feminino">Feminino</option>
-                                    </select>
-                                </div>
-                            </div>
-
-                            <div className=" mb-3 flex flex-row justify-between">
-                                <Dropdown_estados onChange={handleInputChange} />
-                                <Dropdown_cidades state={formValues.state} onChange={handleInputChange} />
-
-                            </div>
-
-                            <div className=" flex w-full items-center justify-center my-5">
-                                <button className=" h-9 w-36 bg-secundaryColor rounded-xl active:bg-primaryColor active:text-secundaryColor" type="submit">
-                                    Enviar
-                                </button>
-                            </div>
-
-                        </form>
+                            )}
+                        </Formik>
 
                     </div>
                 </div>
-
-
-
             </div>
             <Menu_principal />
         </div>
-
+        
     )
 }
