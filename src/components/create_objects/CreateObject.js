1a00fbc39dd31e2a072c578789ef1985612b2d5b
adrianolucasdc <adrianocaixeta5@gmail.com>
2024-05-12 01:43:17 -0300

    feat: CreateObject.jsx para CreateObject.js

--- src/components/create_objects/CreateObject.js
+++ src/components/create_objects/CreateObject.js
@@ -1,22 +1,30 @@
 import { PrismaClient } from "@prisma/client"
-
-
 const prisma = new PrismaClient()
+
 
 export default async function criarUsuario(nome, email, senha, telefone, cpf, cep, dataNasc, sexo, 
     estado, cidade, rua, numero, bairro){
+        const telefoneSemMascara = telefone.replace(/\D/g,"");
+        const cpfSemMascara = cpf.replace(/\D/g, "");
+        const cepSemMascara = cep.replace(/\D/g, "");
+
+        const numeroTelefone = parseInt(telefoneSemMascara);
+        const numeroCpf = parseInt(cpfSemMascara, 10);
+        const numeroCep = parseInt(cepSemMascara, 10);
+        
+
         const create_user = await prisma.usuario.create({
             data:{
                 nome : nome,
                 email : email,
                 senha : senha,
-                telefone : telefone,
-                cpf : cpf,
+                telefone : numeroTelefone,
+                cpf : numeroCpf,
                 dataNasc : dataNasc+"T12:00:00Z",
                 sexo : sexo,
                 endereco : {
                     create : {
-                        cep : cep,
+                        cep : numeroCep,
                         uf : estado,
                         cidade : cidade, 
                         rua : rua,
@@ -26,6 +34,8 @@ export default async function criarUsuario(nome, email, senha, telefone, cpf, ce
                 } 
 
             }
-        })
+        }).catch((error) => {
+            console.error('Erro ao criar usuário:', error);
+        });
     }
 
