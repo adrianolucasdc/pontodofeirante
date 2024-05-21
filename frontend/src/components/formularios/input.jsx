594b1fded4abdbd393e1d7d94575f634190d6914
adrianolucasdc <adrianocaixeta5@gmail.com>
2024-05-21 20:48:03 -0300

    feat: change class field formik

--- frontend/src/components/formularios/input.jsx
+++ frontend/src/components/formularios/input.jsx
@@ -3,14 +3,14 @@ import {Field, ErrorMessage} from "formik"
 
 
 
-export default function campoPreencher(titulo, subtitulo, value, type, borderColor) {
+export default function campoPreencher(titulo, subtitulo, value, type, className) {
 
-    const borderClass = borderColor ? borderColor : 'border-primaryColor';
+    const classNameVar = className ? className : 'border-primaryColor border-2 rounded-md w-full h-[42px] pl-2';
 
     return(
         <div className=" mb-3 flex flex-col">
             <span className=' text-primaryColor font-bold'>{titulo}</span>
-            <Field type={type} name={value} className={` border-2 rounded-md w-40 h-[42px] pl-2 ${borderClass}`}
+            <Field type={type} name={value} className={`${classNameVar}`}
             placeholder={subtitulo}/>
             <div className='overflow-scroll no-scrollbar'>
                 <ErrorMessage name={value} component="div" className='error text-red-600 text-sm ml-3'/>
