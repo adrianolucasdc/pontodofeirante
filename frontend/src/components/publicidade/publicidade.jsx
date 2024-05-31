5bb35c74d1b0d7effc38c6ae13f783f365b0b05b
adrianolucasdc <adrianocaixeta5@gmail.com>
2024-05-31 18:13:22 -0300

    feat: add color to text

--- frontend/src/components/publicidade/publicidade.jsx
+++ frontend/src/components/publicidade/publicidade.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import banner from "../../assets/slnwyvff.png"
 
+
+
 export default function Publicidade() {
     return(
         <div>
             <div className=" w-[370px] h-[150px] bg-thirdColor rounded-2xl">
                 <img src={banner} className='rounded-2xl' alt="" />
             </div>
-
-
         </div>
         
     )
